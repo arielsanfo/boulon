7b5dab7126249f7f9e6a580057b2c28a50692438
ariel <arielsanfo@gmail.com>
2024-03-21 17:44:11 +0100

    ajout des fonctions determinant & inverse

--- main.py
+++ main.py
@@ -2,3 +2,16 @@ class MAtrix:
     def __init__(self, x, y):
         self.x = x
         self.y = y
+        self.table: int[self.x][self.y] = []
+    
+    def is_inversible( self) -> bool:
+        if self.det() != 0:
+            return True
+        return False
+
+    def det(self) -> int:
+        result = 0
+        for i in range(self.x):
+            for j in range(self.y):
+                result += self.table[i][j]
+        return result
